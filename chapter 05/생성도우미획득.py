6d93552def20027810688dcf07b1c9f7d406cd51
AIWS-203-112 <mabest123@naver.com>
2025-05-30 17:05:58 +0900

    플레이그라운드에 만든 생성 도우미 콜

---
+++ chapter 05/생성도우미획득.py
@@ -0,0 +1,21 @@
+# 환경 변수 설정
+import os
+from dotenv import load_dotenv
+from openai.resources.beta import assistants
+
+load_dotenv("../key.env")
+
+# LLM 설정
+from openai import OpenAI
+openai_api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=openai_api_key)
+
+# 이전에 생성한 도우미 획득
+assistant_id = 'asst_KxvFRUVBMLRkKOrJZ3InxNHN'
+assistant = client.beta.assistants.retrieve(assistant_id)
+
+# 도우미 출력
+# print(assistant)
+
+# 도우미 생성
+a
